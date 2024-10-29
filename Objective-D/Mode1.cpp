484d20f6966145003b2e68bb28266d30ba16cffa
TU-PJY <viska0319@icloud.com>
2024-10-29 11:13:41 +0900

    모드 템플릿 업데이트

--- Objective-D/Mode1.cpp
+++ Objective-D/Mode1.cpp
@@ -5,10 +5,11 @@
 
 // 해당 cpp파일과 h파일은 특정한 모드를 실행하고, 해당 모드에 존재하는 객체들을 컨트롤하기 위한 파일이다.
 // 반드시 cpp, h파일로 분리되어있어야 하며, 각 모드에 따라 네임스페이스로 구분되어야한다.
-// 게임_모드::게임모드() 식으로 모드 함수 네임스페이스와 모드 이름을 작성하는 것을 권장한다
+// 게임모드::Start() 식으로 모드 함수 네임스페이스와 모드 이름을 작성하는 것을 권장한다
 // 아래와 같은 형태의 모드 실행 함수 및 컨트롤러 함수를 작성해주기만 하면 프레임워크가 알아서 해준다.
 // 프로그램 시작 시 아래의 함수들은 [ DirectX 3D ] -> [ Main ] -> Scene.cpp에 있는 Init() 함수에서 실행된다.
-// 관련 내용은 Scene.cpp에서 설명하도록 한다.둗
+// 또는 모드 변경 시 실행된다.
+// 관련 내용은 Scene.cpp에서 설명하도록 한다.
 
 // 간편한 모드 코드 작성을 위해 [ Template ] 필터에 템플릿을 만들어 두었으니 복붙한 후 함수 이름과 네임스페이스 이름을 바꾸면 된다.
 
@@ -16,7 +17,7 @@ void Mode1::Start() {
 	// 필요한 객체 추가
 	scene.AddObject(new TestObject, "test_object", LAYER_1);
 	
-	// scene에 컨트롤러 등록
+	// scene에 컨트롤러 및 모드 소멸자 등록
 	RegisterController();
 
 	// 모드 이름을 Scene에 등록
@@ -52,13 +53,10 @@ void Mode1::MouseController(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM lP
 	scene.InputMouse(hWnd, nMessageID, wParam, lParam, "test_object");
 }
 
+// scene에 컨트롤러 및 모드 소멸자 등록
 void Mode1::RegisterController() {
-	// 키보드 컨트롤러 함수 포인터를 Scene에 등록
 	scene.RegisterKeyController(KeyboardController);
-	// 마우스 컨트롤러 함수 포인터를 Scene에 등록
 	scene.RegisterMouseController(MouseController);
-	// 마우스 모션 컨트롤러 포인터를 Scene에 등록
 	scene.RegisterMouseMotionController(MouseMotionController);
-	// 모드 소멸자를 Scene에 등록
 	scene.RegisterDestructor(Destructor);
 }
