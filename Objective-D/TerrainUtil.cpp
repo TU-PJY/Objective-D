4e5aaa3065a39dfc693747fefc371205c275c0c1
TU-PJY <viska0319@icloud.com>
2024-11-10 12:19:18 +0900

    터레인 유틸의 내부 위치값과 오프셋 값을 사용하도록 변경

--- Objective-D/TerrainUtil.cpp
+++ Objective-D/TerrainUtil.cpp
@@ -20,32 +20,26 @@ void TerrainUtil::InputData(XMFLOAT4X4& TMat, XMFLOAT4X4& RMat, XMFLOAT4X4& SMat
 	TerrainMesh->SetHeightCache(TerrainMesh, TerrainMatrix);
 }
 
+// 현재 위치와 높이 오프셋을 입력한다
+void TerrainUtil::InputPosition(XMFLOAT3& PositionValue, float HeightOffsetValue) {
+	Position = PositionValue;
+	HeightOffset = HeightOffsetValue;
+}
+
+// 터레인의 높이로 객체 높이 위치를 변경한다.
+void TerrainUtil::SetHeightToTerrain(XMFLOAT3& PositionValue) {
+	PositionValue.y = Position.y;
+}
+
 // 특정 오브젝트의 위치를 터레인 바닥의 위치로 이동시킨다.
 // 특정 오브젝트가 터레인의 바닥에 닿았는지 검사한다. 닿았을 경우 true를 리턴한다.
-bool TerrainUtil::CheckFloor(const TerrainUtil& Other, XMFLOAT3& Position, float HeightOffset) {
+bool TerrainUtil::CheckCollision(const TerrainUtil& Other) {
 	float Height = Other.TerrainMesh->GetHeightAtPosition(Other.TerrainMesh, Position.x, Position.z, Other.TerrainMatrix) + HeightOffset;
 
-	if (Position.y < Height)
+	if (Position.y < Height) {
+		Position.y = Height;
 		return true;
+	}
 
 	return false;
-}
-
-// 이 함수를 업데이트 루프에서 사용할 경우 오브젝트가 터레인 바닥에서 떨어지지 않게 된다.
-void TerrainUtil::ClampToFloor(const TerrainUtil& Other, XMFLOAT3& Position, float HeightOffset) {
-	Position.y = Other.TerrainMesh->GetHeightAtPosition(Other.TerrainMesh, Position.x, Position.z, Other.TerrainMatrix) + HeightOffset;
-}
-
-// 특정 위치에서의 터레인 높이를 리턴한다.
-float TerrainUtil::GetFloorHeight(const TerrainUtil& Other, XMFLOAT3& Position, float HeightOffset) {
-	float Height = Other.TerrainMesh->GetHeightAtPosition(Other.TerrainMesh, Position.x, Position.z, Other.TerrainMatrix) + HeightOffset;
-	return Height;
-}
-
-// 특정 오브젝트의 위치가 터레인 바닥에 도달할 경우 터레인 바닥의 높이로 오브젝트를 이동시킨다
-void TerrainUtil::CheckCollision(const TerrainUtil& Other, XMFLOAT3& Position, float HeightOffset) {
-	float Height = Other.TerrainMesh->GetHeightAtPosition(Other.TerrainMesh, Position.x, Position.z, Other.TerrainMatrix) + HeightOffset;
-
-	if (Position.y < Height)
-		Position.y = Height;
 }
--- Objective-D/TerrainUtil.h
+++ Objective-D/TerrainUtil.h
@@ -9,9 +9,12 @@ public:
 	XMFLOAT4X4 TerrainMatrix = Mat4::Identity();
 	Mesh* TerrainMesh{};
 
+private:
+	XMFLOAT3 Position{};
+	float HeightOffset{};
+
 	void InputData(XMFLOAT4X4& TMat, XMFLOAT4X4& RMat, XMFLOAT4X4& SMat, Mesh* MeshData);
-	void ClampToFloor(const TerrainUtil& Other, XMFLOAT3& Position, float HeightOffset = 0.0);
-	void CheckCollision(const TerrainUtil& Other, XMFLOAT3& Position, float HeightOffset=0.0);
-	bool CheckFloor(const TerrainUtil& Other, XMFLOAT3& Position, float HeightOffset = 0.0);
-	float GetFloorHeight(const TerrainUtil& Other, XMFLOAT3& Position, float HeightOffset = 0.0);
+	void InputPosition(XMFLOAT3& PositionValue, float HeightOffsetValue=0.0);
+	void SetHeightToTerrain(XMFLOAT3& PositionValue);
+	bool CheckCollision(const TerrainUtil& Other);
 };
