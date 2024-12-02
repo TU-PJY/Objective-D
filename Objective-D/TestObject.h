4b63fd69c0fdc1272eba4fd7b68a0379571238e7
TU-PJY <viska0319@icloud.com>
2024-12-02 11:49:43 +0900

    sync

--- Objective-D/TestObject.h
+++ Objective-D/TestObject.h
@@ -58,20 +58,20 @@ public:
 		Render3D(SkyboxMesh, SkyboxTex, 1.0, false);*/
 
 		// 모델 출력
-		//InitRenderState(RENDER_TYPE_3D);
-		//Transform::Move(TranslateMatrix, Position.x, Position.y, Position.z);
-		//Transform::Rotate(RotateMatrix, Rotation.x, Rotation.y, 0.0);
-		//Transform::Scale(ScaleMatrix, 0.4, 0.4, 0.4);
-		//FlipTexture(FLIP_TYPE_V);
-		//Render3D(GunMesh, Tex);
+		InitRenderState(RENDER_TYPE_3D);
+		Transform::Move(TranslateMatrix, Position.x, Position.y, Position.z);
+		Transform::Rotate(RotateMatrix, Rotation.x, Rotation.y, 0.0);
+		Transform::Scale(ScaleMatrix, 0.4, 0.4, 0.4);
+		FlipTexture(FLIP_TYPE_V);
+		Render3D(GunMesh, Tex);
 
-		//oobb.Update(GunMesh, TranslateMatrix, RotateMatrix, ScaleMatrix);
-		//oobb.Render();
+		oobb.Update(GunMesh, TranslateMatrix, RotateMatrix, ScaleMatrix);
+		oobb.Render();
 
-		//// 이미지 출력, 이미지 종횡비가 자동으로 적용된다.
-		//InitRenderState(RENDER_TYPE_2D);
-		//Transform::Move2D(TranslateMatrix, -0.5, 0.5);
-		//Transform::Scale2D(ScaleMatrix, 0.5, 0.5);
-		//Render2D(WoodTex);
+		// 이미지 출력, 이미지 종횡비가 자동으로 적용된다.
+		InitRenderState(RENDER_TYPE_2D);
+		Transform::Move2D(TranslateMatrix, -0.5, 0.5);
+		Transform::Scale2D(ScaleMatrix, 0.5, 0.5);
+		Render2D(WoodTex);
 	}
 };
